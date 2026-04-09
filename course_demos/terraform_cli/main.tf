5644121fccec828c81cfc94487b35866d8f5f838
Thaddeus <thaddeuspearson@gmail.com>
2026-04-09 18:47:16 +0200

    ud: terraform_cli/main.tf, + var num_of_pets

--- course_demos/terraform_cli/main.tf
+++ course_demos/terraform_cli/main.tf
@@ -1,7 +1,13 @@
 provider "random" {}
 
+variable "num_of_pets" {
+  type        = number
+  description = "how many pets do we want"
+  default     = 3
+}
+
 resource "random_pet" "name" {
-  length = 2
+  length = var.num_of_pets
 }
 
 output "random_pet_name" {
