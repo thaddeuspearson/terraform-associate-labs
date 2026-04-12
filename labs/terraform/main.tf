f2d991c84d6f250cbfb6829fd3a99a8e027bf458
Thaddeus <thaddeuspearson@gmail.com>
2026-04-12 18:21:22 +0200

    ud: main.tf,variables.tf, Lab 3, parameterize CIDR and environment tags

--- labs/terraform/main.tf
+++ labs/terraform/main.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "192.168.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
     Name        = "terraform-course"
-    Environment = "learning-terraform"
+    Environment = var.environment
     Managed_By  = "Terraform"
   }
 }
