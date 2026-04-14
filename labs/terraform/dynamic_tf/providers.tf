877048686685b83dc764356081cb9fa49f682286
Thaddeus <thaddeuspearson@gmail.com>
2026-04-14 20:27:56 +0200

    + dynamic_tf files, Lab 6 Complete

---
+++ labs/terraform/dynamic_tf/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Managed_By = "Terraform"
+      Project    = "Terraform Training"
+    }
+  }
+}
